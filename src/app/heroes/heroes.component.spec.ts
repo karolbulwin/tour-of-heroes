4480c6e75c718a482cacdbbbf37248604457836a
Karol Bulwin <karolbulwin@gmail.com>
2019-07-31 14:37:38 +0200

    added test

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,25 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroesComponent } from './heroes.component';
 
+import { HEROES } from '../mock-heroes';
+import { of } from 'rxjs';
+// import { RouterTestingModule } from '@angular/router/testing';
+
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
-  let fixture: ComponentFixture<HeroesComponent>;
+  // let fixture: ComponentFixture<HeroesComponent>;
+  let heroes;
+  let mockHeroService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HeroesComponent ]
-    })
-    .compileComponents();
-  }));
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [HeroesComponent],
+  //     imports: [RouterTestingModule]
+  //   }).compileComponents();
+  // }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeroesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture = TestBed.createComponent(HeroesComponent);
+    // component = fixture.componentInstance;
+    // fixture.detectChanges();
+    heroes = HEROES;
+
+    mockHeroService = jasmine.createSpyObj([
+      'getHero',
+      'addHero',
+      'deleteHero'
+    ]);
+
+    component = new HeroesComponent(mockHeroService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('deleteHero method', () => {
+    it('should remove the indicated hero from the heros list', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = heroes;
+      const heroToDelete = heroes[1];
+
+      component.deleteHero(heroToDelete);
+
+      expect(component.heroes.length).toBe(heroes.length - 1);
+      expect(
+        component.heroes.filter(hero => hero.id === heroToDelete.id).length
+      ).toBe(0);
+    });
   });
 });
