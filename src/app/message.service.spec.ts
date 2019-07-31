ddd01e970087c0d4b251fa34192a5eee779048be
Karol Bulwin <karolbulwin@gmail.com>
2019-07-31 13:19:54 +0200

    added tests

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -3,10 +3,44 @@ import { TestBed } from '@angular/core/testing';
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: MessageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    // service = new MessageService(); // add to it for better story // arrange // act // assert
+  });
 
   it('should be created', () => {
-    const service: MessageService = TestBed.get(MessageService);
+    // const service: MessageService = TestBed.get(MessageService);
+    service = new MessageService();
+
     expect(service).toBeTruthy();
+  });
+
+  it('should have no messages on start', () => {
+    service = new MessageService();
+
+    expect(service.messages.length).toBe(0);
+  });
+
+  describe('add method', () => {
+    it('should add a message when add is called', () => {
+      service = new MessageService();
+
+      service.add('Hello!');
+
+      expect(service.messages.length).toBe(1);
+    });
+  });
+
+  describe('clear method', () => {
+    it('should clear all messages when clear is called', () => {
+      service = new MessageService();
+      service.add('Hello!');
+
+      service.clear();
+
+      expect(service.messages.length).toBe(0);
+    });
   });
 });
