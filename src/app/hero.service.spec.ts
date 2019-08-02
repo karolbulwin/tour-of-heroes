b9a2f2b94c9b0842c7914039b61789fc9e20a2a3
Karol Bulwin <karolbulwin@gmail.com>
2019-08-02 11:34:33 +0200

    test config

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,12 +1,64 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+  TestRequest
+} from '@angular/common/http/testing';
 
 import { HeroService } from './hero.service';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 describe('HeroService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let heroService: HeroService;
+  let httpTestingController: HttpTestingController;
+  let mockMessageService;
 
-  it('should be created', () => {
-    const service: HeroService = TestBed.get(HeroService);
-    expect(service).toBeTruthy();
+  let testHeroes: Hero[] = HEROES;
+
+  beforeEach(() => {
+    mockMessageService = jasmine.createSpyObj(['add, clear']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        HeroService,
+        { provide: MessageService, useValue: mockMessageService }
+      ]
+    });
+
+    heroService = TestBed.get(HeroService);
+    httpTestingController = TestBed.get(HttpClientTestingModule);
   });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  // describe('getHeroes method', () => {
+  //   it('should get all heroes', () => {
+  //     heroService.getHeroes().subscribe((data: Hero[]) => {
+  //       expect(data.length).toBe(testHeroes.length);
+  //     });
+
+  //     const heroesRequest: TestRequest = httpTestingController.expectOne(
+  //       '/api/heroes'
+  //     );
+  //     expect(heroesRequest.request.method).toEqual('GE');
+
+  //     heroesRequest.flush(testHeroes);
+  //   });
+  // });
+  // describe('getHero method', () => {
+  //   it('should call get with the correct URL', () => {
+  //     heroService.getHero(1).subscribe();
+
+  //     const heroRequest: TestRequest = httpTestingController.expectOne(
+  //       'api/heroes/1'
+  //     );
+
+  //     heroRequest.flush(testHeroes[1]);
+  //   });
+  // });
 });
