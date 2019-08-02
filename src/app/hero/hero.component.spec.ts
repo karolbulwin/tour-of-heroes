41180c1d95b979910914e9983fa509ae21604efb
Karol Bulwin <karolbulwin@gmail.com>
2019-08-02 10:41:01 +0200

    added tests

--- src/app/hero/hero.component.spec.ts
+++ src/app/hero/hero.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroComponent } from './hero.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeroComponent', () => {
   let component: HeroComponent;
@@ -8,18 +9,28 @@ describe('HeroComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroComponent ]
-    })
-    .compileComponents();
+      declarations: [HeroComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have the correct hero', () => {
+    component.hero = { id: 1, name: 'Karol' };
+
+    expect(component.hero.name).toEqual('Karol');
+  });
+
+  it('should render the hero name in an anchor tag', () => {
+    component.hero = { id: 1, name: 'Karol' };
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('a').textContent).toContain(
+      'Karol'
+    );
   });
 });
