9729e59044a33f6b3b375f4c8a841b2a5dba999f
Karol Bulwin <karolbulwin@gmail.com>
2019-08-02 13:18:57 +0200

    added test

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,25 +1,57 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroDetailComponent } from './hero-detail.component';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common';
+import { of } from 'rxjs';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  let mockActivatedRoute;
+  let mockHeroService;
+  let mockLocation;
 
   beforeEach(async(() => {
+    mockActivatedRoute = {
+      snapshot: {
+        paramMap: {
+          get: () => {
+            return '2';
+          }
+        }
+      }
+    };
+    mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']);
+    mockLocation = jasmine.createSpyObj(['back']);
+
     TestBed.configureTestingModule({
-      declarations: [ HeroDetailComponent ]
-    })
-    .compileComponents();
+      declarations: [HeroDetailComponent],
+      imports: [FormsModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: HeroService, useValue: mockHeroService },
+        { provide: Location, useValue: mockLocation }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroDetailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('getHero method', () => {
+    it('should render hero name in a h2 tag', () => {
+      mockHeroService.getHero.and.returnValue(of({ id: 2, name: 'Karol' }));
+      fixture.detectChanges();
+
+      expect(fixture.nativeElement.querySelector('h2').textContent).toContain(
+        'KAROL'
+      );
+    });
   });
 });
