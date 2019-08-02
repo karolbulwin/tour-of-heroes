7ccd6f1fbb6b22128c11ccb48bdfaa2ba49a223b
Karol Bulwin <karolbulwin@gmail.com>
2019-08-02 13:20:03 +0200

    config test

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroSearchComponent } from './hero-search.component';
+import { HeroService } from '../hero.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
   let fixture: ComponentFixture<HeroSearchComponent>;
+  let mockHeroService;
 
   beforeEach(async(() => {
+    mockHeroService = jasmine.createSpyObj(['searchHeroes']);
+
     TestBed.configureTestingModule({
-      declarations: [HeroSearchComponent]
+      declarations: [HeroSearchComponent],
+      imports: [RouterTestingModule],
+      providers: [{ provide: HeroService, useValue: mockHeroService }]
     }).compileComponents();
   }));
 
