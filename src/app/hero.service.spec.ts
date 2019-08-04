545f2a19f2098bccdfefa24524edf0b45560e4b2
Karol Bulwin <karolbulwin@gmail.com>
2019-08-04 14:50:00 +0200

    added tests

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -18,7 +18,7 @@ describe('HeroService', () => {
   let testHeroes: Hero[] = HEROES;
 
   beforeEach(() => {
-    mockMessageService = jasmine.createSpyObj(['add, clear']);
+    mockMessageService = jasmine.createSpyObj(['add', 'clear']);
 
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -29,36 +29,96 @@ describe('HeroService', () => {
     });
 
     heroService = TestBed.get(HeroService);
-    httpTestingController = TestBed.get(HttpClientTestingModule);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   afterEach(() => {
     httpTestingController.verify();
   });
 
-  // describe('getHeroes method', () => {
-  //   it('should get all heroes', () => {
-  //     heroService.getHeroes().subscribe((data: Hero[]) => {
-  //       expect(data.length).toBe(testHeroes.length);
-  //     });
+  it('should be created', () => {
+    const service: HeroService = TestBed.get(HeroService);
+    expect(service).toBeTruthy();
+  });
 
-  //     const heroesRequest: TestRequest = httpTestingController.expectOne(
-  //       '/api/heroes'
-  //     );
-  //     expect(heroesRequest.request.method).toEqual('GE');
+  describe('getHeroes method', () => {
+    it('should get all heroes', () => {
+      heroService.getHeroes().subscribe((data: Hero[]) => {
+        expect(data.length).toBe(testHeroes.length);
+      });
 
-  //     heroesRequest.flush(testHeroes);
-  //   });
-  // });
-  // describe('getHero method', () => {
-  //   it('should call get with the correct URL', () => {
-  //     heroService.getHero(1).subscribe();
+      const heroesRequest: TestRequest = httpTestingController.expectOne(
+        'api/heroes'
+      );
+      expect(heroesRequest.request.method).toEqual('GET');
 
-  //     const heroRequest: TestRequest = httpTestingController.expectOne(
-  //       'api/heroes/1'
-  //     );
+      heroesRequest.flush(testHeroes);
+    });
+  });
+  describe('getHero method', () => {
+    it('should call get with the correct URL', () => {
+      const heroToGet = testHeroes[0];
+      heroService.getHero(heroToGet.id).subscribe((data: Hero) => {
+        expect(data).toBe(testHeroes[0]);
+      });
 
-  //     heroRequest.flush(testHeroes[1]);
-  //   });
-  // });
+      const heroesRequest: TestRequest = httpTestingController.expectOne(
+        `api/heroes/${heroToGet.id}`
+      );
+
+      expect(heroesRequest.request.method).toEqual('GET');
+
+      heroesRequest.flush(testHeroes[0]);
+    });
+  });
+
+  describe('addHero method', () => {
+    it('should add new hero to the heroes list', () => {
+      const newHero: Hero = { id: 1, name: 'Karol' };
+      heroService.addHero(newHero).subscribe((data: Hero) => {
+        expect(data).toEqual(newHero);
+      });
+
+      const heroesRequest: TestRequest = httpTestingController.expectOne(
+        'api/heroes'
+      );
+      expect(heroesRequest.request.method).toEqual('POST');
+
+      heroesRequest.flush(newHero);
+    });
+  });
+
+  describe('updateHero method', () => {
+    it('should update hero with new name', () => {
+      const newHero: Hero = { id: 11, name: 'Karol' };
+      heroService.updateHero(newHero).subscribe((data: Hero) => {
+        expect(data).toEqual(newHero);
+      });
+
+      const heroesRequest: TestRequest = httpTestingController.expectOne(
+        'api/heroes'
+      );
+
+      expect(heroesRequest.request.method).toEqual('PUT');
+
+      heroesRequest.flush(newHero);
+    });
+  });
+
+  describe('deleteHero method', () => {
+    it('should delete current hero', () => {
+      const heroToDelete = testHeroes[0];
+      heroService.deleteHero(heroToDelete).subscribe((data: Hero) => {
+        expect(data).toEqual(heroToDelete);
+      });
+
+      const heroesRequest: TestRequest = httpTestingController.expectOne(
+        `api/heroes/${heroToDelete.id}`
+      );
+
+      expect(heroesRequest.request.method).toEqual('DELETE');
+
+      heroesRequest.flush(heroToDelete);
+    });
+  });
 });
